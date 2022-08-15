5bd0f71a456984a4b99cefc67fbac333c1b02e4e
giovanni neves sadauscas <giovanni_neves@tutanota.com>
2022-08-15 16:50:48 -0300

    Atualizando páginas

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./tailwind.js"></script>
+    <title>{{$_ENV['APP_NAME']}} Blog</title>
+</head>
+<body>
+    <h1 class="text-center display-1">Hello, Blog</h1>
+</body>
+</html>
---
+++ resources/views/post.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../tailwind.js"></script>
+    <title>{{$_ENV['APP_NAME']}} Post</title>
+</head>
+
+<body class="">
+    <div class="mt-2">
+        <article>
+            <?= $post ?>
+        </article>
+        <a href="/blog">Go back</a>
+    </div>
+</body>
+
+</html>
