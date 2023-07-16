c55c9bdc04104ba8ac3ceddc7610763e1c8748c2
Giovanni <giovanni_neves@tutanota.com>
2023-07-16 13:54:52 -0300

    Adicionando nova sessão de contato

--- index.php
+++ index.php
@@ -387,10 +387,18 @@
                             desenvolvimento e a manutenção de aplicativos web modernos e responsivos. </p>
                     </div>
                 </div>
-
             </article>
+
         </section>
-        <section class="bg-">
+        <section class="bg-orange">
+            <div class="custom-shape-divider-top-1689523377">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
+                    preserveAspectRatio="none">
+                    <path d="M1200,0H0V120H281.94C572.9,116.24,602.45,3.86,602.45,3.86h0S632,116.24,923,120h277Z"
+                        class="shape-fill"></path>
+                </svg>
+            </div>
+
             <h2 class="is-1 ps-2">Contato</h2>
         </section>
     </main>
