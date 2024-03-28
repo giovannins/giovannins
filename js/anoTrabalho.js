bba035700793ded50b02bae5d8a52b085a7b9d5f
Giovanni Neves <giovanni_neves@tutanota.com>
2024-03-28 09:57:09 -0300

    Excluindo antigo porfolio

--- js/anoTrabalho.js
+++
@@ -1,6 +0,0 @@
-const anoTrabalhoElement = document.getElementById("anoTrabalho");
-const date = new Date();
-const anoAtual = date.getFullYear();
-const anoTrabalho = (anoAtual - 2017);
-
-anoTrabalhoElement.innerText = anoTrabalho;
